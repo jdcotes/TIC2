acc6d70a3b9fd9d5cc21701bb34757bb461a898f
jdcotes <jdcotes@uninorte.edu.co>
2017-05-27 16:55:00 -0500

    prueba tabla

--- getTweets.php
+++ getTweets.php
@@ -11,7 +11,7 @@ class Twitter{
                         'consumer_secret' => "IeeGjaLauKK1X9rSgd2uPlfF9kdNf9Tcidr2HQHWFtvAmQL73k");
 
       $url = 'https://api.twitter.com/1.1/search/tweets.json';
-      $getfield = '?geocode=10.9838039,-74.8880581,20km';
+      $getfield = '?geocode=10.9838039,-74.8880581,10km';
       $requestMethod = 'GET';
       $twitter = new TwitterAPIExchange($settings);
       $json = $twitter->setGetfield($getfield)
@@ -19,8 +19,59 @@ class Twitter{
                 ->performRequest();
       return $json;
   }
+      function getInfoTwitter($contenedorJson){
+
+      $count = 0;
+      $rawdata = "";
+      $json = "";
+      for($i=0;$i<count($contenedorJson);$i++){
+        
+        $json = $contenedorJson[$i];
+        $json = json_decode($json);
+        $num_items = count($json->statuses);
+
+        for ($j=0; $j<$num_items; $j++){
+          $user = $json->statuses[$j];
+          $id_tweet = $user->id_str;
+          $fecha = $user->created_at;
+          $url_imagen = $user->user->profile_image_url;
+          $screen_name = $user->user->screen_name;
+          $imagen = "<a href='https://twitter.com/".$screen_name."' target=_blank><img src=".$url_imagen."></img></a>";
+          $tweet = $user->text;
+
+          if(!empty($user->geo->coordinates[0])){
+            $latitud = $user->geo->coordinates[0];
+            $longitud = $user->geo->coordinates[1];
+          }
+          else{
+            $latitud = 0;
+            $longitud = 0;
+          }
+          $rawdata[$count][0] = $fecha;
+          $rawdata[$count]["FECHA"] = $fecha;
+          $rawdata[$count][1] = $imagen;
+          $rawdata[$count]["imagen"] = $imagen;
+          $rawdata[$count][3] = $url_imagen;
+          $rawdata[$count]["imagen_url"] = $url_imagen; 
+          $rawdata[$count][4]="@".$screen_name;
+                $rawdata[$count]["nombre"]="@".$screen_name;
+                $rawdata[$count][5]=$tweet;
+                $rawdata[$count]["tweet"]=$tweet;
+                $rawdata[$count][6]=$latitud;
+                $rawdata[$count]["latitud"]=$latitud;
+                $rawdata[$count][7]=$longitud;
+                $rawdata[$count]["longitud"]=$longitud;
+                $count++;
+        }
+      }
+      print_r($rawdata);
+      print_r(count($rawdata));
+      return $rawdata;  
+    }
 }
   $twitterObject = new Twitter();
   $jsonraw = $twitterObject->getTweets();
-  echo($jsonraw);
+  $rawdata = $twitterObject->fdf_get_encoding($jsonraw);
+  echo($rawdata);
+
 ?>
