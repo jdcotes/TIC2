94f34a43d7caa8dd62383ecc0a45aadc7fc3ebd8
jdcotes <jdcotes@uninorte.edu.co>
2017-05-27 22:22:12 -0500

    echo

--- getTweets.php
+++ getTweets.php
@@ -78,14 +78,15 @@ class Twitter{
            echo "<th><b>".key($rawdata[0])."</b></th>";
            next($rawdata[0]);
       }
-      for($i=0;$i<$columnas;$i++){
-           echo "<tr>";
-           for($j=0;$j<$filas;$j++){
-               echo "<td>".$rawdata[$i][$j]."</td>";
-           }
-           echo "</tr>";
+      //for($i=0;$i<$filas;$i++){
+      //     echo "<tr>";
+      //     for($j=0;$j<$columnas;$j++){
+      //         echo "<td>".$rawdata[$i][$j]."</td>";
+      //     }
+      //     echo "</tr>";
       }
       echo '</table>';
+      print_r($rawdata[0][1]);
   }
 }
   $twitterObject = new Twitter();
